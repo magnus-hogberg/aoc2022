765b364336f76b19ce0b219c3f1840ab81869cc0
Magnus Högberg <magnus.h@gmail.com>
2022-11-27 01:08:34 +0100

    Bump version

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+set -x
+
 source version.txt
 
 docker build -t cloud.canister.io:5000/topz/aoc2022:latest -t cloud.canister.io:5000/topz/aoc2022:$MAJOR.$MINOR Aoc2022
+cat docker-compose.yml.template \
+| sed "s/\$MINOR/${MINOR}/g" \
+| sed "s/\$MAJOR/${MAJOR}/g" \
+> Aoc2022/docker-compose.yml
+
