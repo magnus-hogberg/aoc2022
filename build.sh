9c093a43a36b66b70cc2b2d4d70901088fb67c8c
Magnus Högberg <magnus.h@gmail.com>
2022-12-01 07:43:03 +0100

    Dec 1

--- build.sh
+++ build.sh
@@ -6,7 +6,6 @@ source version.txt
 
 docker build -t cloud.canister.io:5000/topz/aoc2022:latest -t cloud.canister.io:5000/topz/aoc2022:$MAJOR.$MINOR Aoc2022
 cat docker-compose.yml.template \
-| sed "s/\$MINOR/${MINOR}/g" \
-| sed "s/\$MAJOR/${MAJOR}/g" \
-> Aoc2022/docker-compose.yml
-
+  | sed "s/\$MINOR/${MINOR}/g" \
+  | sed "s/\$MAJOR/${MAJOR}/g" \
+  > Aoc2022/docker-compose.yml
