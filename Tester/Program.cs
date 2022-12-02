937838556d1d670817743a73bd293d5765441a3c
Magnus Högberg <magnus.h@gmail.com>
2022-12-02 08:25:44 +0100

    Solution dec 2

---
+++ Tester/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Aoc2022.Solutions;
+
+var sol = new SolutionDec2();
+Console.WriteLine(sol.Solution2());
---
+++ push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker image push --all-tags cloud.canister.io:5000/topz/aoc2022
